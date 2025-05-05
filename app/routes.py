3c586b6129a1d1804de4a94baef0837585cd382a
CChris152 <1650306859@qq.com>
2025-05-05 23:47:20 +0800

    add 完善model和数据库连接

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,24 @@
 from flask import Blueprint, request, jsonify
 
+from app.model.user import User
+
 bp = Blueprint('main', __name__)
 
 
 @bp.route('/user/login', methods=['POST'])
 def user_login():
-    return '111'
+    # 查询所有用户
+    users = User.query.all()
+
+    # 将用户数据转换为字典格式
+    user_list = [{
+        'id': user.id,
+        'username': user.username,
+        'avatar_url': user.avatar_url,
+        'email': user.email
+    } for user in users]
+
+    return {'users': user_list}
 
 
 @bp.route('/user/register', methods=['POST'])
--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:root@localhost/deepfake'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:1234@127.0.0.1:3306/deepfake'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
