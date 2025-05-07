aec8b076b7cda8f69a165a659aaea7ad712de70a
CChris152 <1650306859@qq.com>
2025-05-07 19:34:11 +0800

    add 完善注册和检测记录接口

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
+import secrets
+
 class Config:
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:1234@127.0.0.1:3306/deepfake'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = "secret key"
+    JWT_SECRET_KEY = secrets.token_hex(32)
